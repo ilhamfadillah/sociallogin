9137eb3a32bd66f1d7cbce1aa2e0454d9e34ec86
ilham_fadillah <ilhamfadillah1995@gmail.com>
2018-06-21 18:51:38 +0700

    test

--- app/Http/Controllers/SocialAuthFacebookController.php
+++ app/Http/Controllers/SocialAuthFacebookController.php
@@ -26,8 +26,14 @@ class SocialAuthFacebookController extends Controller
     public function callback(SocialFacebookAccountService $service)
     {
       $user = $service->createOrGetUser(Socialite::driver('facebook')->user());
-      var_dump($user->getAccessToken()); exit;
+      $x = Socialite::driver('facebook')->user();
+      var_dump($user->token);
       echo "<br><hr>";
+      var_dump($user->refreshToken);
+      echo "<br><hr>";
+      var_dump($user->tokenSecret); exit;
+      echo "<br><hr>";
+      var_dump($x->token); exit;
         auth()->login($user);
         return redirect()->to('/home');
 
